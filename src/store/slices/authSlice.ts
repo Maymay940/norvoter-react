5c9d31a8f19fe2f2d5442e173736caf7cbbde08b
Irina <qpqpqpfrt@gmail.com>
2026-05-26 16:48:24 +0700

    проба гитхаб пейджерс6

--- src/store/slices/authSlice.ts
+++ src/store/slices/authSlice.ts
@@ -2,7 +2,7 @@ import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
 // Определяем режим GitHub Pages
-const isGitHubPages = import.meta.env.MODE === 'production';
+const isGitHubPages = true;
 
 console.log('isGitHubPages =', import.meta.env.VITE_IS_GITHUB_PAGES);
 console.log('MODE =', import.meta.env.MODE);
